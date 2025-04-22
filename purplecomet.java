049f618f9a58cb77234321d90ea5c852c9356540
ajing60 <audrey.y.jing@gmail.com>
2025-04-22 20:21:33 +0000

    fix

--- purplecomet.java
+++ purplecomet.java
@@ -1,21 +1,25 @@
 public class purplecomet {
     public static void main (String[] args) {
         double num = Math.pow (199, 2);
+        int sum = 0;
         for (int i = 2; i < 199; i += 3) {
             num += Math.pow (i, 2) - 4 * i;
         }
         System.out.println (num);
-        if (num % 2 == 0) {
-            System.out.println ("2 is a factor");
+        while (num % 2 == 0) {
+            System.out.println ("2");
             num /= 2;
+            sum += 2;
         }
-        System.out.println (num);
-        System.out.println (Math.sqrt (num));
         for (int k =3 ;k < Math.sqrt (num);  k += 2 ){
-            if (num % k == 0){
+            while (num % k == 0){
                 System.out.println (k );
                 num /= k;
+                sum += k;
             }
         }
+        System.out.println (num);
+        sum += num;
+        System.out.rpintln (sum);
     }
 }
