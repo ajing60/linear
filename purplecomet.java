29ea046c52058fe07da1611b135da616b68c968b
ajing60 <audrey.y.jing@gmail.com>
2025-04-22 20:13:14 +0000

    fix

--- purplecomet.java
+++ purplecomet.java
@@ -1,9 +1,10 @@
 public class purplecomet {
     public static void main (String[] args) {
-        int num = 199^2;
+        int num = Math.pow (199, 2);
         for (int i = 2; i < 199; i += 3) {
-            num += i^2 - 4 * i;
+            num += Math.pow (i, 2) - 4 * i;
         }
+        
         if (num % 2 == 0) {
             System.out.println ("2 is a factor");
         }
