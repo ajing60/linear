98a16b0ee968f992cb29f7481a79db810eb18ed3
ajing60 <audrey.y.jing@gmail.com>
2025-04-22 20:10:24 +0000

    compile

--- purplecomet.java
+++ purplecomet.java
@@ -1,1 +1,17 @@
-
+public class purplecomet {
+    public static void main (String[] args) {
+        int num = 199^2;
+        for (int i = 2; i += 3; i < 199) {
+            num += i^2 - 4i;
+        }
+        if (num % 2 == 0) {
+            System.out.println ("2 is a factor");
+        }
+        for (int k =2 ; k += 2; k < Math.sqrt (k)){
+            if (num % k == 0){
+            System.out.println (k );
+            num /= k;
+            }
+        }
+    }
+}
