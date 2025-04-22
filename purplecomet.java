60b899de80abddcb4a20dbb35c0fbc0a77144689
ajing60 <audrey.y.jing@gmail.com>
2025-04-22 20:17:48 +0000

    fix

--- purplecomet.java
+++ purplecomet.java
@@ -1,6 +1,6 @@
 public class purplecomet {
     public static void main (String[] args) {
-        double num = Math.pow (2, 0);
+        double num = Math.pow (12, 1);
         for (int i = 2; i < 199; i += 3) {
             num += Math.pow (i, 2) - 4 * i;
         }
